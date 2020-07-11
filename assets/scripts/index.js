a4f3ce9b0b91ae1dca7c3b3c051c75b206a4805f
Christian Paranas <christiannparanas@gmail.com>
2020-07-12 02:45:20 +0800

    con

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,14 +1,21 @@
 new Vue({
    el: '#app',
    data: {
+      pre: true,
       pre1: true,
-      pre2: false
+      pre2: false,
+      home: false
    },
    methods: {
       preload: function() {
          setTimeout(() => {
             this.pre1 = false
             this.pre2 = true
+
+            setTimeout(() => {
+               this.pre = false
+               this.home = true
+            }, 2500)
 
         }, 2000)
       }
